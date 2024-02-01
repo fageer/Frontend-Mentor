13f2a64237d12382f39507481daaa347ec126557
fageer <fageern@gmail.com>
2024-02-01 15:08:11 +0300

    modifying

--- base-apparel-coming-soon-master/main.js
+++ base-apparel-coming-soon-master/main.js
@@ -11,7 +11,6 @@ form.addEventListener("submit", (e) => {
   }
 });
 function validateEmail(email) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re =/\w+@\w+.(com|net)/g;
   return re.test(email);
 }
